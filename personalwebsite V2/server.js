23800b6de201c3d950ac84f62dc76aa1e2598c37
KaiZhang2020 <78572839+KaiZhang2020@users.noreply.github.com>
2023-02-07 16:35:45 -0800

    initial

---
+++ personalwebsite V2/server.js
@@ -0,0 +1,17 @@
+const express = require('express') 
+const app = express()
+const port = 5000
+
+//static files
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/public", express.static(__dirname + "/public"));
+//end of static files
+
+app.get("/", (req,res) => {
+    res.redirect("/public/index.html")
+})
+
+app.listen(port, () => {
+    console.log("listening on port " + port)
+})
